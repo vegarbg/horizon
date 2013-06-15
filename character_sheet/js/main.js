f585a93f9b3d9a830e752bd205c5427f7e1c4ac2
Vegar Guldal <vegar@guldal.no>
2013-06-15 16:51:10 +0200

    Add function to determine stat bonus from value.

--- character_sheet/js/main.js
+++ character_sheet/js/main.js
@@ -3,6 +3,46 @@ function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+function statBonus(statValue) {
+    if(!isNumber(statValue)) {
+        return false;
+    }
+    switch(+statValue) { // cast to number
+        case 0:
+            return "–";
+            break;
+        case 1:
+            return -5;
+            break;
+        case 2:
+            return -3;
+            break;
+        case 3:
+            return 0;
+            break;
+        case 4:
+            return 1;
+            break;
+        case 5:
+            return 3;
+            break;
+        case 6:
+            return 5;
+            break;
+        case 7:
+            return 9;
+            break;
+        case 8:
+            return 15;
+            break;
+        case 9:
+            return 21;
+            break;
+        default:
+            return false;
+    }
+}
+
 function AppViewModel() {
     this.statStrength = ko.observable();
     this.statEndurance = ko.observable();
