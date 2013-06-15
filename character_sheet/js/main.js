2900b116a97ec608959bc264f1f25c15c72728e6
Vegar Guldal <vegar@guldal.no>
2013-06-15 16:28:15 +0200

    Revert commits.
    This reverts the following commits:
    * "Adjust indentation."
      33a2882fcece469704af75a7baae8545fb4d589f
    * "Wrap JS with jQuery."
      418f24569aa4be18b0956943fccd72baf9d28e2e

--- character_sheet/js/main.js
+++ character_sheet/js/main.js
@@ -1,21 +1,19 @@
-$(function() {
-    function AppViewModel() {
-        this.statStrength = ko.observable();
-        this.statEndurance = ko.observable();
-        this.statAgility = ko.observable();
-        this.statSpeed = ko.observable();
+function AppViewModel() {
+    this.statStrength = ko.observable();
+    this.statEndurance = ko.observable();
+    this.statAgility = ko.observable();
+    this.statSpeed = ko.observable();
 
-        this.statWillpower = ko.observable();
-        this.statInsight = ko.observable();
-        this.statReasoning = ko.observable();
-        this.statPerception = ko.observable();
+    this.statWillpower = ko.observable();
+    this.statInsight = ko.observable();
+    this.statReasoning = ko.observable();
+    this.statPerception = ko.observable();
 
-        this.statPresence = ko.observable();
-        this.statComposure = ko.observable();
-        this.statManipulation = ko.observable();
-        this.statBeauty = ko.observable();
-    }
+    this.statPresence = ko.observable();
+    this.statComposure = ko.observable();
+    this.statManipulation = ko.observable();
+    this.statBeauty = ko.observable();
+}
 
-    // Activate knockout.js
-    ko.applyBindings(new AppViewModel());
-});
+// Activate knockout.js
+ko.applyBindings(new AppViewModel());
