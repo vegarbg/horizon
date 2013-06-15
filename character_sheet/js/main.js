965c3288ebf2b3bef73854cded6ca3aa8b5ea1bc
Vegar Guldal <vegar@guldal.no>
2013-06-15 15:36:43 +0200

    Add observable properties for all char attributes.

--- character_sheet/js/main.js
+++ character_sheet/js/main.js
@@ -1,1 +1,19 @@
+function AppViewModel() {
+    this.statStrength = ko.observable();
+    this.statEndurance = ko.observable();
+    this.statAgility = ko.observable();
+    this.statSpeed = ko.observable();
 
+    this.statWillpower = ko.observable();
+    this.statInsight = ko.observable();
+    this.statReasoning = ko.observable();
+    this.statPerception = ko.observable();
+
+    this.statPresence = ko.observable();
+    this.statComposure = ko.observable();
+    this.statManipulation = ko.observable();
+    this.statBeauty = ko.observable();
+}
+
+// Activate knockout.js
+ko.applyBindings(new AppViewModel());
