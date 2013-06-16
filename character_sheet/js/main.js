08eaf2ffcb2bd521d792290a858b6687562fdfbc
Vegar Guldal <vegar@guldal.no>
2013-06-16 16:27:56 +0200

    Add skill_groups block with names of all skills.

--- character_sheet/js/main.js
+++ character_sheet/js/main.js
@@ -72,6 +72,176 @@ var AppViewModel = {
                 { name: "Beauty" }
             ]
         }
+    ],
+    skill_groups: [
+        {
+            name: "Uncategorized",
+            skills: [
+                { name: "Awareness" },
+                { name: "Sense Motive" },
+                { name: "Dodge" },
+                { name: "Focus" }
+            ]
+        },
+        {
+            name: "Athletics",
+            skills: [
+                { name: "Climb" },
+                { name: "Jump" },
+                { name: "Contort" },
+                { name: "Swim" },
+                { name: "Balance" },
+                { name: "Acrobatics" }
+            ]
+        },
+        {
+            name: "Armour",
+            skills: [
+                { name: "Light Armour" },
+                { name: "Medium Armour" },
+                { name: "Heavy Armour" }
+            ]
+        },
+        {
+            name: "Martial Arts",
+            skills: [
+                { name: "Soft Fall" },
+                { name: "Sweeping Throw" },
+                { name: "Punches" },
+                { name: "Kicks" },
+                { name: "Locking Grip" }
+            ]
+        },
+        {
+            name: "Subterfuge, Social",
+            skills: [
+                { name: "Bluff" },
+                { name: "Diplomacy" },
+                { name: "Intimidate" },
+                { name: "Seduce" },
+                { name: "Disguise" }
+            ]
+        },
+        {
+            name: "Subterfuge, Mechanical",
+            skills: [
+                { name: "Forgery" },
+                { name: "Open Lock" },
+                { name: "Disable Trap" },
+                { name: "Sneak" }
+            ]
+        },
+        {
+            name: "Craft, Mechanical",
+            skills: [
+                { name: "Locksmith" },
+                { name: "Clockmaker" },
+                { name: "Trapmaking" }
+            ]
+        },
+        {
+            name: "Craft, Construction",
+            skills: [
+                { name: "Carpentry" },
+                { name: "Masonry" }
+            ]
+        },
+        {
+            name: "Medicine",
+            skills: [
+                { name: "Treat Wounds" },
+                { name: "Herbalism" }
+            ]
+        },
+        {
+            name: "Perform",
+            skills: [
+                { name: "Music [instrument]" },
+                { name: "Oratory" },
+                { name: "Theatre" },
+                { name: "Buffoonery" }
+            ]
+        },
+        {
+            name: "Academics",
+            skills: [
+                { name: "Knowledge (Religion)" },
+                { name: "Knowledge (History)" },
+                { name: "Knowledge (Anatomy)" },
+                { name: "Knowledge (Magic)" }
+            ]
+        },
+        {
+            name: "Blades",
+            skills: [
+                { name: "Knife" },
+                { name: "Short Sword" },
+                { name: "Long Sword" },
+                { name: "Greatsword" },
+                { name: "Rapier" },
+                { name: "Axe" },
+                { name: "Greataxe" }
+            ]
+        },
+        {
+            name: "Blunt",
+            skills: [
+                { name: "Club" },
+                { name: "Greatclub" },
+                { name: "Flail" },
+                { name: "Baton" },
+                { name: "Sap" },
+                { name: "Axe" },
+                { name: "Greataxe" }
+            ]
+        },
+        {
+            name: "Bows",
+            skills: [
+                { name: "Footbow" },
+                { name: "Longbow" },
+                { name: "Shortbow" },
+                { name: "Crossbow" }
+            ]
+        },
+        {
+            name: "Thrown",
+            skills: [
+                { name: "Knife" },
+                { name: "Sling" },
+                { name: "Spear" },
+                { name: "Darts" },
+                { name: "Shuriken" },
+                { name: "Boomerang" },
+                { name: "Bolas" }
+            ]
+        },
+        {
+            name: "Polearms",
+            skills: [
+                { name: "Halberd" },
+                { name: "Spear" },
+                { name: "Lance" },
+                { name: "Quarterstaff" },
+                { name: "Three-part Staff" }
+            ]
+        },
+        {
+            name: "Flexible Weapons",
+            skills: [
+                { name: "Rope Dart" },
+                { name: "Chain" },
+                { name: "Whip" }
+            ]
+        },
+        {
+            name: "Animal Husbandry",
+            skills: [
+                { name: "Handle Animal" },
+                { name: "Ride" },
+                { name: "Animal Empathy" }
+            ]
+        }
     ]
 };
 
