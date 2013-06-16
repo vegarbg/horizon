c577401617ad9958d98cb5dab8d48575aa8d003f
Vegar Guldal <vegar@guldal.no>
2013-06-16 03:07:16 +0200

    Replace AppViewModel function with object.

--- character_sheet/js/main.js
+++ character_sheet/js/main.js
@@ -43,27 +43,8 @@ function statBonus(statValue) {
     }
 }
 
-function AppViewModel() {
-    this.statStrength = ko.observable();
-    this.statEndurance = ko.observable();
-    this.statAgility = ko.observable();
-    this.statSpeed = ko.observable();
-
-    this.statWillpower = ko.observable();
-    this.statInsight = ko.observable();
-    this.statReasoning = ko.observable();
-    this.statPerception = ko.observable();
-
-    this.statPresence = ko.observable();
-    this.statComposure = ko.observable();
-    this.statManipulation = ko.observable();
-    this.statBeauty = ko.observable();
-
-    this.statBonusStrength = ko.computed(function() {
-        return statBonus(this.statStrength());
-    }, this);
-
-    this.characterAttributes = [
+var AppViewModel = {
+    characterAttributes: [
         {
             name: "Physical",
             attributes: [
@@ -127,8 +108,9 @@ function AppViewModel() {
                 }
             ]
         }
-    ];
-}
+    ]
+};
 
 // Activate knockout.js
-ko.applyBindings(new AppViewModel());
+ko.applyBindings(AppViewModel);
+
